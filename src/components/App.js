993ffab2086cb0fb9e5325276d9f8f3707ad5448
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-09-18 20:24:37 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./../styles/App.css";
 
 const App = () => {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const handleInput = (e) => {
+    const task = {
+      name: e.target.value,
+      id: uuidv4(),
+    };
+    setTask(task);
+  };
+  const handleDelete = (id) => {
+    const newlist = list.filter((item) => item.id !== id);
+    setList(newlist);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>Todo List</h1>
+      <input onChange={handleInput} value={task} />
+      <button
+        onClick={() => {
+          setList([...list, task]);
+          setTask("");
+        }}
+      >
+        Add Todo
+      </button>
 
-export default App
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.name}
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
